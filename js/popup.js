ed894af4dff8956354c8e15e03cbd4d1c0ed8861
mariya-polyakova1991 <mariyaluk@yandex.ru>
2020-07-14 13:30:19 +0300

    add popup

---
+++ js/popup.js
@@ -0,0 +1,30 @@
+var popup = document.querySelector(".modal");
+var openPopupButton = document.querySelector(".button-open");
+var closePopupButton = popup.querySelector(".button-close");
+const commentList = document.getElementById("comment-list");
+const commentField = document.getElementById("comment-field");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+
+openPopupButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal--show");
+  commentList.classList.add("comment-list");
+  commentList.textContent =
+    "Your name: " +
+    name.value +
+    ", Your email: " +
+    email.value +
+    ", Your message: " +
+    commentField.value;
+});
+
+closePopupButton.addEventListener("click", function () {
+  popup.classList.remove("modal--show");
+});
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    popup.classList.remove("modal--show");
+  }
+});
